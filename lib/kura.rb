9729ab3b74a0afdf6912c6c2ff6db29c9b2cfefd
Daniel Lobato <elobatocs@gmail.com>
2012-12-11 21:43:34 +0100

    Handles files/directories differently

--- lib/kura.rb
+++ lib/kura.rb
@@ -1,10 +1,22 @@
 class Kura
-  def initialize(*argv)
-    @argv = argv
+  attr_reader :input
+
+  def initialize(input)
+    @input = input
     failure_message = "What do you want to build? Examples:
                     one package: kura package.src.rpm
                   many packages: kura packages_dir/"  
-    puts failure_message if @argv.empty?
+    if @input.empty?
+      puts failure_message 
+      #exit
+    else
+      start_process
+    end
+  end
+
+  def start_process
+    @input = @input.to_s
+    File.directory?(@input) ? DirectoryBuilder.new(@input) : PackageBuilder.new(@input)
   end
 
 end
--- spec/unit/kura_spec.rb
+++ spec/unit/kura_spec.rb
@@ -3,19 +3,45 @@ require 'kura'
 
 describe 'kura' do
   context 'user interaction' do
-    before(:each) { output_file = StringIO.new }
-
     it 'should require at least one argument, directory or package' do
       failure_message = "What do you want to build? Examples:
                     one package: kura package.src.rpm
                   many packages: kura packages_dir/\n" 
-      output = capture_stdout { Kura.new }
+      output = fake_stdout { Kura.new("") }
       output.should == failure_message
     end
 
     it "should not print any error message with a proper argument" do
-      output = capture_stdout { Kura.new("package.src.rpm") }
-      output.should == ""
+      InputFaker.with_fake_input(["owner", "tag"]) do 
+        output = fake_stdout { Kura.new("package-0.0.0-1.src.rpm") }
+        output.should_not =~ /What do you want to build/
+      end
+    end
+  end
+
+  context "files and directories" do
+    it "should use PackageBuilder for files" do
+      InputFaker.with_fake_input(["owner", "tag"]) do 
+        package = "package-0.0.0-1.src.rpm"
+        fake_stdout do 
+          kura = Kura.new(package)  
+          DirectoryBuilder.should_not_receive(:new)
+          PackageBuilder.should_receive(:new).with(package)
+          kura.start_process
+        end
+      end
+    end
+
+    it "should use DirectoryBuilder for dirs" do
+      InputFaker.with_fake_input(["owner", "tag"]) do 
+        dir = "spec/sample_dir"
+        fake_stdout do 
+          kura = Kura.new(dir)  
+          DirectoryBuilder.should_receive(:new).with(dir)
+          PackageBuilder.should_not_receive(:new)
+          kura.start_process
+        end
+      end                                   
     end
   end
 end
